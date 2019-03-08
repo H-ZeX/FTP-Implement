1196744e62ebacd6a41a906b4fac16374a599460
H-ZeX <hzx20112012@sina.cn>
2019-03-08 19:22:44 +0800

    refactore util files
    Refator util files.This version can not be built

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hzx/MyStudy/OS/FTP/Server
-# Build directory: /home/hzx/MyStudy/OS/FTP/Server
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(UTest "UtilTest")
